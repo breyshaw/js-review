0c94252f84a14849706e843c24fc7dd78f787fa0
breyshaw <dawsonbrey@gmail.com>
2022-03-30 13:07:29 -0400

    add operator notes

--- js-control-flow/js-control-flow.js
+++ js-control-flow/js-control-flow.js
@@ -1,1 +1,50 @@
-console.log('sanity check')
+//Control Flow is the execution sequence of instructions in a program determined at run time using control structures
+
+//if then
+//If Boots eats tacos
+//Then Boots is Happy
+
+//Basic types of Control Flow
+
+//Sequence - If x then y 
+//Branching - if not x then y or else z
+//Looping - if not x, then keep going until you hit x
+
+//if statement essentially is checking for truth
+
+if (undefined) {
+  console.log('TRUTHY')
+} else {
+  console.log('FALSEY')
+}
+
+//Everything in JS is truthy except two data types: null & defined and four values: false, 0, NaN and an empty string ""
+
+//The not operator or "bang" operator, "flips" a true or truthy expression to the boolean value of false, and vice-versa.
+//A double! is a good way to force an expression into it's actual boolean value
+
+// = is used to assign variables (assignment operator)
+// === strict equality
+console.log(3 === 4)
+// == checks for abstract equality or type conversion
+console.log(3 == '3')
+// strict inequality !==
+// inequality !=
+
+//Logical Operators
+
+// || or operator
+// the OR operator always returns the FIRST operand if it is truthy; OTHERWISE the second operand is returned - REGARDLESS of whether it is truthy or falsy
+
+console.log(
+  'party' || 'goodbye'
+)
+
+console.log(
+  0 || null
+)
+
+// && and operator - always returns the first operand if it is falsy, otherwise the second will be returned - regardless of truthyness or falsyness
+
+console.log( 'party' && 'goodbye')
+console.log( 0 && null)
