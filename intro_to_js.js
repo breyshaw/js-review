c6f973b8c3251b11346dd2215c00c202ccd1319d
breyshaw <dawsonbrey@gmail.com>
2022-03-30 11:42:39 -0400

    add var, const, & let code and notes

---
+++ intro_to_js.js
@@ -0,0 +1,40 @@
+// The core of programming (according to Shazad)
+//1. how to allocate memory
+//2. organizing data
+//3. iterate over that data
+
+// JavaScript: multi-paradigm, high level, interpreted language
+
+// Variables
+// Used to reference data
+// var, let, and const
+// the difference between var & let/const(ES6) is SCOPE
+// var has some vulnerabilites
+// avoid using var
+
+// var party = 'tonight'
+// console.log(party)
+
+let party = 'tonight is the night!'
+console.log(party)
+
+// let allows you to reassign values
+// const protects you from reassigning
+
+let a = 25
+console.log(a)
+
+a = 55
+console.log(a)
+
+const b = 100
+console.log(b)
+
+b = 121
+console.log(b)
+
+// let a = 25
+// let b = a
+// a = 11
+
+// b stays 25
