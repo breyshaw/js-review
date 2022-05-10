440ce47f86c87b9b32b1b856521b752789dee909
breyshaw <dawsonbrey@gmail.com>
2022-05-10 14:11:55 -0400

    add object, property & methods code and notes

--- js-objects/app.js
+++ js-objects/app.js
@@ -1,1 +1,21 @@
-console.log('sanity check')
+// Each data type has some built in methods. Examples:
+  // string - toUpperCase(), toLowerCase()
+  // number - parseInt()
+  // arrays - push, pop, forEach, unshift, splice
+  // functions - 3 ways to define, rest parameters
+  // objects
+    // Almost everything in JS is an object
+    // JS is a huge object
+
+// Global Object (Node environment)
+  // Window object (chrome browser)
+
+  // OOP - Object Oriented Programming
+  // Objects have state and behavior
+  // Object | State | Behavior
+  // jsObject | Property | Methods
+
+  const s = 'Hey, what up?'
+  console.log(s.length) //accessing the current length property of the string
+
+  console.log(s.toLowerCase()) // changing the state of the string by using the toLowerCase method/behavior
